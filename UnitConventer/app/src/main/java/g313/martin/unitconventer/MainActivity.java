571ad790c7bc55d900c900cc9698510bae2db7f8
Ярослав <yaroslavmartin05.gmail.com>
2023-11-12 18:43:11 +0300

    UnitConverter
    Конвертирует всякое

---
+++ UnitConventer/app/src/main/java/g313/martin/unitconventer/MainActivity.java
@@ -0,0 +1,69 @@
+package g313.martin.unitconventer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    Spinner spn_from;
+    Spinner spn_to;
+    EditText txt_from;
+    TextView txt_to;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        spn_from = findViewById(R.id.spn_from);
+        spn_to = findViewById(R.id.spn_to);
+        txt_from = findViewById(R.id.txt_from);
+        txt_to = findViewById(R.id.txt_to);
+
+        ArrayAdapter <String> adp = new <String> ArrayAdapter(this, android.R.layout.simple_list_item_1);
+
+        adp.add("mm");
+        adp.add("cm");
+        adp.add("m");
+        adp.add("km");
+
+        spn_from.setAdapter(adp);
+        spn_to.setAdapter(adp);
+    }
+
+    public void on_convert(View v)
+    {
+        try
+        {
+            double from = Double.parseDouble(txt_from.getText().toString());
+
+            String s_from = (String) spn_from.getSelectedItem();
+            String s_to = (String)  spn_to.getSelectedItem();
+
+            double to = 0.0f;
+            double m = 0.0f;
+
+            if (s_from.equals("mm")) m = from / 1000.0f;
+            if (s_from.equals("cm")) m = from / 100.0f;
+            if (s_from.equals("m")) m = from;
+            if (s_from.equals("km")) m = from * 1000.0f;
+
+            if (s_to.equals("mm")) to = m * 1000.0f;
+            if (s_to.equals("cm")) to = m * 100.0f;
+            if (s_to.equals("m")) to = m;
+            if (s_to.equals("km")) to = m / 1000.0f;
+
+            txt_to.setText(String.valueOf(to));
+        }
+        catch (Exception e)
+        {
+            txt_to.setText("Error");
+        }
+    }
+}
